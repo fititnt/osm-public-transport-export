fa67b26783fe5de7809eff47bc2b71eac402ae25
Emerson Rocha <rocha@ieee.org>
2022-10-06 22:43:44 -0300

    Recife-Brasil started; first test (no lib code changes, only bbox)

--- examples/Recife-Brasil/index.js
+++ examples/Recife-Brasil/index.js
@@ -10,11 +10,18 @@ osmToGeojson({
     }),
     stopNameSeparator: ' and ',
     stopNameFallback: 'unnamed',
+    // osmDataGetter: new OSMOverpassDownloader({
+    //     south: 17.958761,
+    //     west: -16.025151,
+    //     north: 18.192123,
+    //     east: -15.874505,
+    // })
+    // https://nominatim.openstreetmap.org/search?q=Recife,Pernambuco&format=geojson
     osmDataGetter: new OSMOverpassDownloader({
-        south: 17.958761,
-        west: -16.025151,
-        north: 18.192123,
-        east: -15.874505,
+        south: -35.0186478,
+        west: -8.1551869,
+        north: -34.8515401,
+        east: -7.928967
     })
 })
     .catch(error => console.error(error))
---
+++ examples/Recife-Brasil/out/README.md
@@ -0,0 +1,6 @@
+### Count
+**Total**: 0  **Correct**: 0  **With error**: 0
+
+
+| Id | Name | Ref | From | To | State |
+| -- | ---- | --- | ---- | -- | ----- |
